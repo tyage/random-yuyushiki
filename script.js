d5eda9a48db71c40ee997a4bb93e022aab2e0ad7
tyage <namatyage@gmail.com>
2014-10-13 04:04:54 +0900

    rehash url

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 $(function() {
+  var query = decodeURIComponent(location.search).substring(1);
+  var params = {};
+  query.split('&').forEach(function(param) {
+    var p = param.split('=');
+    params[p[0]] = p[1];
+  });
+  var rehash = function() {
+    history.pushState(null, null, '?' + $.param(params));
+  };
+  var setImageParam = function(index, image) {
+    var images = params.images.split(',');
+    images[index] = image;
+    params.images = images.join(',');
+  };
+
   $.getJSON('yuyushiki/yuyushiki.json').done(function(data) {
     var usedData = _.filter(data, function(image) {
       return !image.useless;
@@ -10,25 +25,24 @@ $(function() {
     var generateImages = function() {
       var images = [];
       for (var i = 0; i < 4; ++i) {
-        images.push(randomImage());
+        var image = randomImage();
+        images.push(image);
+        setImageParam(i, image);
       }
+      rehash();
       return images;
     };
-    var query = decodeURIComponent(location.search).substring(1);
-    var params = {};
-    query.split('&').forEach(function(param) {
-      var p = param.split('=');
-      params[p[0]] = p[1];
-    });
     var images = params.images ? params.images.split(',') : generateImages();
     var base = params.base || '';
 
     $('#images').empty();
-    _.each(images, function(image) {
+    _.each(images, function(image, i) {
       var url = base + data[image].path;
       $('<img>').attr('src', url).click(function() {
-        var url = base + data[randomImage()].path;
+        var id = randomImage();
+        var url = base + data[id].path;
         $(this).attr('src', url);
+        setImageParam(i, id);
       }).appendTo('#images');
     });
   });
