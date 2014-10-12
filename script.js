8e7a9854a829060d3d60a2303566bec4fd05109e
tyage <namatyage@gmail.com>
2014-10-13 02:39:09 +0900

    split params to base, images

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ $(function() {
   var generateImages = function() {
     return [];
   };
-  var images = location.search.length > 0 ?
-    location.search.slice(1).split(',') : generateImages();
+  var query = decodeURIComponent(location.search).substring(1);
+  var params = {};
+  query.split('&').forEach(function(param) {
+    var p = param.split('=');
+    params[p[0]] = p[1];
+  });
+  var images = params.images.length > 0 ? params.images.split(',') : generateImages();
+  var base = params.base || '';
 
   $('#images').empty();
   images.forEach(function(image) {
-    $('<img>').attr('src', image).appendTo('#images');
+    $('<img>').attr('src', base + image).appendTo('#images');
   });
 });
