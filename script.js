a7a405e150bc99d1bb4e78f315efa4f6256334f3
tyage <namatyage@gmail.com>
2014-10-13 02:17:13 +0900

    generate random images when query is empty

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 $(function() {
-  var images = location.search.split('?')[1].split(',');
+  var generateImages = function() {
+    return [];
+  };
+  var images = location.search.length > 0 ?
+    location.search.slice(1).split(',') : generateImages();
+
   $('#images').empty();
   images.forEach(function(image) {
     $('<img>').attr('src', image).appendTo('#images');
